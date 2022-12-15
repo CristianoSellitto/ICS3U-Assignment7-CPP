bc82f9ebc3ce79d79d6f2efd1443dd6b628daebc
Cristiano Sellitto <cristiano.sellitto@mths.ca>
2022-12-15 16:20:47 +0000

    Changed program

--- addingNumbersInArrays.cpp
+++ addingNumbersInArrays.cpp
@@ -7,23 +7,59 @@
 
 #include <iostream>
 #include <string>
+#include <array>
+
+template <size_t length>
+int AddNumbersInArrays(std::array<int, length> array, int numberOfLoops) {
+    int sum = 0;
+
+    for (int counter = 0; counter < numberOfLoops; counter++) {
+        sum += array[counter];
+    }
+
+    return sum;
+}
+
+template <size_t length>
+std::string FormatAddingArrayText(
+    std::array<int, length> array,
+    int numberOfLoops) {
+    std::string equationText = "";
+
+    for (int counter = 0; counter < numberOfLoops; counter++) {
+        if (counter == numberOfLoops - 1) {
+            equationText += std::to_string(array[counter]) + " = ";
+        } else {
+            equationText += std::to_string(array[counter]) + " + ";
+        }
+    }
+
+    return equationText;
+}
 
 int main() {
     // Adds all of the numbers in an array and prints it back to the user
 
-    int numberOfLoopsInteger;
-    int finalAnswer = 0;
+    int numberOfLoopsInteger = 0;
+    int finalAnswer;
     std::string numberOfLoopsText;
-    std::string fullEquationText = "";
+    std::string fullEquationText;
 
     // Set the number of loops if it is a valid integer
     while (true) {
         try {
-            std::cout << "Enter the amount of numbers you want in the array: ";
+            std::cout << "Enter the amount of numbers";
+            std::cout << "you want in the array (max = 20): ";
             std::cin >> numberOfLoopsText;
             std::cout << std::endl;
             numberOfLoopsInteger = stoi(numberOfLoopsText);
-            break;
+            if (numberOfLoopsInteger <= 20) {
+                break;
+            } else {
+                std::cout << "You did not enter a valid integer.";
+                std::cout << std::endl;
+                std::cout << std::endl;
+            }
         } catch (std::invalid_argument) {
             std::cout << "You did not enter a valid integer.";
             std::cout << std::endl;
@@ -31,7 +67,7 @@ int main() {
         }
     }
     // Append the inputted number to the array if it is a valid integer
-    int numberArray[512];
+    std::array<int, 20> numberArray;
     for (int counter = 0; counter < numberOfLoopsInteger; counter++) {
         try {
             float numberValue;
@@ -55,20 +91,8 @@ int main() {
         }
     }
     // Write the full equation and calculate the answer
-    for (int counter = 0; counter < numberOfLoopsInteger; counter++) {
-        if (counter == numberOfLoopsInteger - 1) {
-            fullEquationText = (
-                fullEquationText +
-                std::to_string(numberArray[counter]) +
-                " = ");
-        } else {
-            fullEquationText = (
-                fullEquationText +
-                std::to_string(numberArray[counter]) +
-                " + ");
-        }
-        finalAnswer = finalAnswer + numberArray[counter];
-    }
+    finalAnswer = AddNumbersInArrays(numberArray, numberOfLoopsInteger);
+    fullEquationText = FormatAddingArrayText(numberArray, numberOfLoopsInteger);
     // Print the full equation and the final answer as a string
     std::cout << fullEquationText << finalAnswer;
     std::cout << std::endl;
